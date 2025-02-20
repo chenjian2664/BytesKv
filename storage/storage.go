99916d0c0917b334b23d4107ad4eaeda1f4a46ed
chenjian2664 <chenjian2664@gmail.com>
2025-02-20 17:09:22 +0800

    Add storage api

---
+++ storage/storage.go
@@ -0,0 +1,18 @@
+package storage
+
+type StorageManager interface {
+	// Read from file with the position
+	Read([]byte, int64) (int, error)
+
+	// Write to the file with the position
+	Write([]byte) (int, error)
+
+	// Flush refresh memo data into storage
+	Flush() error
+
+	// Close the storage manager
+	Close() error
+
+	// Size Get File size
+	Size() (int64, error)
+}
